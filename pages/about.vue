bd9d4f6a9b9e8136be6ce00d5cfc1df765c425b9
Clark Feusier <cfeusier@gmail.com>
2018-10-21 23:06:39 -0700

    CF: about page partial progress

--- pages/about.vue
+++ pages/about.vue
@@ -1,120 +1,168 @@
 <template>
   <section>
     <h1 class="primary--text text--darken-1 display-4">About</h1>
-    <v-card tile class="mt-20 mb-20 p-10">
-      <p>
-        <v-icon left>place</v-icon>
-        <span>
-          I work as a <strong>Software Architect and Engineer</strong> located in <em>California</em>
-        </span>
-      </p>
-      <p>
-        <v-icon left>school</v-icon>
-        <span>
-          <em>B.A.</em> from <strong>California Polytechnic State University</strong>, San Luis Obispo &mdash; studies in <a href="http://en.wikipedia.org/wiki/Philosophy" target="_blank">philosophy</a> and <a href="http://en.wikipedia.org/wiki/Physics" target="_blank" rel="noopener">physics</a>
-        </span>
-      </p>
-      <p>
-        <v-icon left>library_books</v-icon>
-        <span>
-          <em>M.A.</em> from the <strong>University of Chicago</strong> &mdash; research in <a href="http://en.wikipedia.org/wiki/Metalogic" target="_blank">metalogic</a> and <a href="http://en.wikipedia.org/wiki/Philosophy_of_science" target="_blank" rel="noopener">philosophy of science</a>
-        </span>
-      </p>
+    <v-card tile class="mt-20 mb-20">
+      <v-toolbar color="secondary" flat card dense height="15"></v-toolbar>
+      <v-toolbar color="primary" flat card dense height="5"></v-toolbar>
+      <v-card-text class="primary--text text--darken-1">
+        <p>
+          <v-icon left class="primary--text text--lighten-1">place</v-icon>
+          <span>
+            I work as a <strong>Software Architect and Engineer</strong> located in <em>California</em>
+          </span>
+        </p>
+        <p>
+          <v-icon left class="primary--text text--lighten-1">school</v-icon>
+          <span>
+            <em>B.A.</em> from <strong>California Polytechnic State University</strong>, San Luis Obispo &mdash; studies in <a href="http://en.wikipedia.org/wiki/Philosophy" target="_blank">philosophy</a> and <a href="http://en.wikipedia.org/wiki/Physics" target="_blank" rel="noopener">physics</a>
+          </span>
+        </p>
+        <p>
+          <v-icon left class="primary--text text--lighten-1">account_balance</v-icon>
+          <span>
+            <em>M.A.</em> from the <strong>University of Chicago</strong> &mdash; research in <a href="http://en.wikipedia.org/wiki/Metalogic" target="_blank">metalogic</a> and <a href="http://en.wikipedia.org/wiki/Philosophy_of_science" target="_blank" rel="noopener">philosophy of science</a>
+          </span>
+        </p>
+      </v-card-text>
     </v-card>
-    <v-divider></v-divider>
-    <h2>Software Engineering</h2>
-    <ul class="about-list">
-      <li>
-        <a href="#" class="disabledAnchor"><i class="fa fa-code big-icon"></i></a>
-        <p>
-          Software engineering satiates my desire to solve large problems with magic &mdash; symbols, semantic reference, circuitry, and a dash of electricity. I am rarely more satisfied than when building software.
-        </p>
-      </li>
-      <li>
-        <a href="#" class="disabledAnchor"><i class="fa fa-terminal big-icon"></i></a>
-        <p>
-          I tend to view most technologies as disposable, so I focus on the general and invariant principles behind the technologies. That being said, my current interests center around the following:
-        </p>
-        <p>
-          <strong>'Languages':</strong> JavaScript, Ruby, HTML, CSS, Unix Shell, SQL, KIF, HOL
-        </p>
-        <p>
-          <strong>Frameworks and Libraries:</strong> Node, Express, Sinatra, Rails, jQuery, React, Backbone, Angular, Ember, Meteor, Sail, Locomotive
-        </p>
-        <p>
-          <strong>Database Tech:</strong> MongoDB, Redis, PostgreSQL, CouchDB, Neo4j, Cassandra, MySQL
-        </p>
-        <p>
-          <strong>Methodologies:</strong> All things lean, agile, and funky-fresh
-        </p>
-        <p>
-          <strong>Trends:</strong> Cognition as a Service (CaaS), Xiki shell, formal semantic ontology integration into big data work, open knowledge bases, and robots and shit
-        </p>
-        <p>
-          <strong>Currently Exploring:</strong> Go, Scala, Clojure, Python, Java, Swift, C, Cython, Erlang, Lisp
-        </p>
-      </li>
-      <li>
-        <a href="#" class="disabledAnchor"><i class="fa fa-code-fork big-icon"></i></a>
-        <p>
-          My open-source-interests include web application frameworks, dev tools, machine learning libraries, natural language processing libraries, knowledge representation frameworks, and anything cool.
-        </p>
-        <p>
-          I am interested in building products that are novel, helpful to a large portion of society, fun to use, and contain interesting technical problems to solve.
-        </p>
-      </li>
-    </ul>
-    <hr />
-    <h2>Metalogic and Philosophy</h2>
-    <p>
-      It's no surprise that I enjoy building software, given my fascination with artificial symbolic languages. In my studies, this fascination manifested itself as a passion for complex systems, particularly those constructed in symbolic logic. My thought always seems to revolve around topics in and in-between metalogic, science, philosophy, and software engineering.
-    </p>
-    <ul class="about-list">
-      <li><a href="#" class="disabledAnchor"><i class="fa fa-subscript big-icon"></i></a>
-        <p>
-          My metalogical pursuits focus on the construction of 'non-classical' logical systems, particularly deontic, temporal, and modal logics &mdash; I would like to be able to formally specify normative discourse so that computers can capture the large amount of normative talk on the web and infer over it.
-        </p>
-        <p>
-          I also like to get my hands dirty with knowledge representation formats, particularly those based on formal predicate logic. I see the ability to construct bullet-proof semantic ontologies and apply them to our current data as foundational in creating large-scale machine learning and consistent inference. I am particularly enthralled by trying to specify large bodies of law, medical theory and record-data, and public knowledge.
-        </p>
-      </li>
-      <li><a href="#" class="disabledAnchor"><i class="fa fa-puzzle-piece big-icon"></i></a>
-        <p>
-          My thinking styles are heavily influenced by the sciences and philosophies of science. I think this is related to my fascination with complex systems that display emergent properties. I see the world through a filter of relativistic and quantum mechanical physics, neo-evolutionary biological theory, neurophysiological thought, and different perspectives from computer science.
-        </p>
-      </li>
-      <li><a href="#" class="disabledAnchor"><i class="fa fa-book big-icon"></i></a>
-        <p>
-          My 'classic' philosophical desires always come back to the dead German dudes &mdash; Kant, Nietzsche, and Wittgenstein. This is probably also where I get my flare for melodrama and eclectic thought.
-        </p>
-      </li>
-      <li><a href="#" class="disabledAnchor"><i class="fa fa-sitemap big-icon"></i></a>
-        <p>
-          Finally, my philosophical wheelhouse is the pursuit of clarity around normativity, language, mind, science, and logic.
-        </p>
-        <p>
-          All of the above don't interest me in the <em><strong>slightest,</strong></em> relative to the <em><strong>ties</strong></em> between all of the above disciplines &mdash; particularly as they fit into and drive the future of software development.
-        </p>
-      </li>
-    </ul>
-    <hr />
-    <h2>Life <em>sans</em> Software and Philosophy</h2>
-    <ul class="about-list">
-      <li><a href="#" class="disabledAnchor"><i class="fa fa-birthday-cake big-icon"></i></a>
-        <p>
-          I am 26 years old, male, very cool, and very uncool. I am also paradoxical.
-        </p>
-      </li>
-      <li><a href="#" class="disabledAnchor"><i class="fa fa-heart big-icon"></i></a>
-        <div class="fiddleImg">
-          <img class="fiddlePic" src="/img/fiddle.jpg">
+    <v-card tile class="mt-20 mb-20">
+      <v-toolbar color="secondary" flat card dense height="15"></v-toolbar>
+      <v-toolbar color="primary" flat card dense height="5"></v-toolbar>
+      <v-card-text class="primary--text text--darken-1">
+        <h2 class="primary--text text--darken-1 display-1">Software</h2>
+        <div>
+          <v-icon left large class="primary--text text--lighten-1">code</v-icon>
+          <p>
+            Software engineering satiates my desire to solve large problems with magic &mdash; symbols, semantic reference, circuitry, and a dash of electricity. I am rarely more satisfied than when building software.
+          </p>
         </div>
-        <p>
-        My dog <a href="#" class="fiddleLink">Fiddle</a>, piano, wine, beer, coffee, tea, whiskey, bay area sports, music, nice people, smart people, pretty people, cool people, and hiking.
-        </p>
-      </li>
-    </ul>
-    <hr />
+        <div>
+          <v-icon left large class="primary--text text--lighten-1">devices_other</v-icon>
+          <p>
+            I tend to view most technologies as disposable, so I focus on the general and invariant principles behind the technologies. That being said, my current interests center around the following:
+          </p>
+          <p>
+            <strong>'Languages':</strong> JavaScript, Ruby, HTML, CSS, Unix Shell, SQL, KIF, HOL
+          </p>
+          <p>
+            <strong>Frameworks and Libraries:</strong> Node, Express, Sinatra, Rails, jQuery, React, Backbone, Angular, Ember, Meteor, Sail, Locomotive
+          </p>
+          <p>
+            <strong>Database Tech:</strong> MongoDB, Redis, PostgreSQL, CouchDB, Neo4j, Cassandra, MySQL
+          </p>
+          <p>
+            <strong>Methodologies:</strong> All things lean, agile, and funky-fresh
+          </p>
+          <p>
+            <strong>Trends:</strong> Cognition as a Service (CaaS), Xiki shell, formal semantic ontology integration into big data work, open knowledge bases, and robots and shit
+          </p>
+          <p>
+            <strong>Currently Exploring:</strong> Go, Scala, Clojure, Python, Java, Swift, C, Cython, Erlang, Lisp
+          </p>
+        </div>
+        <div>
+          <v-icon left large class="primary--text text--lighten-1">call_split</v-icon>
+          <p>
+            My open-source-interests include web application frameworks, dev tools, machine learning libraries, natural language processing libraries, knowledge representation frameworks, and anything cool.
+          </p>
+          <p>
+            I am interested in building products that are novel, helpful to a large portion of society, fun to use, and contain interesting technical problems to solve.
+          </p>
+        </div>
+      </v-card-text>
+    </v-card>
+    <v-card tile class="mt-20 mb-20">
+      <v-toolbar color="secondary" flat card dense height="15"></v-toolbar>
+      <v-toolbar color="primary" flat card dense height="5"></v-toolbar>
+      <v-card-text class="primary--text text--darken-1">
+        <h2 class="primary--text text--darken-1 display-1">Metalogic and Philosophy</h2>
+        <div>
+          <v-icon left large class="primary--text text--lighten-1">functions</v-icon>
+          <p>
+            It's no surprise that I enjoy building software, given my fascination with artificial symbolic languages. In my studies, this fascination manifested itself as a passion for complex systems, particularly those constructed in symbolic logic. My thought always seems to revolve around topics in and in-between metalogic, science, philosophy, and software engineering.
+          </p>
+          <p>
+            My metalogical pursuits focus on the construction of 'non-classical' logical systems, particularly deontic, temporal, and modal logics &mdash; I would like to be able to formally specify normative discourse so that computers can capture the large amount of normative talk on the web and infer over it.
+          </p>
+          <p>
+            I also like to get my hands dirty with knowledge representation formats, particularly those based on formal predicate logic. I see the ability to construct bullet-proof semantic ontologies and apply them to our current data as foundational in creating large-scale machine learning and consistent inference. I am particularly enthralled by trying to specify large bodies of law, medical theory and record-data, and public knowledge.
+          </p>
+        </div>
+        <div>
+          <v-icon left large class="primary--text text--lighten-1">category</v-icon>
+          <p>
+            My thinking styles are heavily influenced by the sciences and philosophies of science. I think this is related to my fascination with complex systems that display emergent properties. I see the world through a filter of relativistic and quantum mechanical physics, neo-evolutionary biological theory, neurophysiological thought, and different perspectives from computer science.
+          </p>
+        </div>
+        <div>
+          <v-icon left large class="primary--text text--lighten-1">library_books</v-icon>
+          <p>
+            My 'classic' philosophical desires always come back to the dead German dudes &mdash; Kant, Nietzsche, and Wittgenstein. This is probably also where I get my flare for melodrama and eclectic thought.
+          </p>
+        </div>
+        <div>
+          <v-icon left large class="primary--text text--lighten-1">bubble_chart</v-icon>
+          <p>
+            Finally, my philosophical wheelhouse is the pursuit of clarity around normativity, language, mind, science, and logic.
+          </p>
+          <p>
+            All of the above don't interest me in the <em><strong>slightest,</strong></em> relative to the <em><strong>ties</strong></em> between all of the above disciplines &mdash; particularly as they fit into and drive the future of software development.
+          </p>
+        </div>
+      </v-card-text>
+    </v-card>
+    <v-card tile class="mt-20 mb-20">
+      <v-toolbar color="secondary" flat card dense height="15"></v-toolbar>
+      <v-toolbar color="primary" flat card dense height="5"></v-toolbar>
+      <v-card-text class="primary--text text--darken-1">
+        <h2 class="primary--text text--darken-1 display-1">Life Outside the Mind</h2>
+        <div>
+          <v-icon left large class="primary--text text--lighten-1">cake</v-icon>
+          <p>
+            I am 30 years old and male.
+          </p>
+        </div>
+        <no-ssr placeholder="fiddle">
+          <div>
+            <v-icon left large class="primary--text text--lighten-1">pets</v-icon>
+            <div>
+              <v-dialog v-model="fiddle" max-width="50%">
+                <p slot="activator">
+                  My dog, <span class="primary--text text--lighten-1 hover">Fiddle</span>
+                  <span>
+                    is my best friend. She makes every day better. She is 5 years old -- the longest relationship I've ever been had.
+                  </span>
+                </p>
+                <v-card img="fiddle.jpg" height="600" class="primary--border-lighten-1">
+                  <v-btn icon color="secondary" @click.stop="fiddle = false"><v-icon color="primary">cancel</v-icon></v-btn>
+                </v-card>
+              </v-dialog>
+            </div>
+          </div>
+        </no-ssr>
+        <div>
+          <v-icon left large class="primary--text text--lighten-1">favorite</v-icon>
+          <p>
+            My family...
+          </p>
+        </div>
+        <div>
+          <v-icon left large class="primary--text text--lighten-1">music_note</v-icon>
+          <p>
+            Playing: mainly piano...
+          </p>
+        </div>
+        <div>
+          <v-icon left large class="primary--text text--lighten-1">headset</v-icon>
+          <p>
+            Listening: fairly eclectic, but always leaning towards hip-hop, unless feeling nostalgic...
+          </p>
+        </div>
+      </v-card-text>
+    </v-card>
+
+<!--  wine, beer, coffee, tea, whiskey, bay area sports, nice people, smart people, pretty people, cool people, and hiking.</p> -->
+
     <h2>Past Life</h2>
     <h4>Caution: Braggy</h4>
     <ul class="about-list">
@@ -152,4 +200,14 @@
 
   </section>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      fiddle: false
+    };
+  }
+}
+</script>
 
